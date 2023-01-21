1e55789009bbda55660a4ba320df4f2969233de0
Douglas Carvalho <doug.oak598@gmail.com>
2023-01-21 13:14:23 -0300

    add readme

--- script.js
+++ script.js
@@ -28,13 +28,6 @@ function save() {
 
 }
 
- //   const data = {
- //       run: ["01-01","01-02","01-03","01-04","01-05","01-06", "01-07","01-09","01-10","01-11","01-12","01-13","01-14",],
- //       sleep: ["01-03"],
- //       cat: ["01-04"],
-
-//    }
-
 const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData (data)
 nlwSetup.load()
